8621dc0b8159d42861e122e3e22fcc709a05db1c
g2fly <edwardhill91@gmail.com>
2016-05-17 19:05:11 -0400

    adding filter logic

--- www/views/menu/menu.js
+++ www/views/menu/menu.js
@@ -1,39 +1,42 @@
 'Use Strict';
-angular.module('App').controller('menuController', function ($scope, $ionicHistory, $ionicSideMenuDelegate, $state,$cordovaOauth, $localStorage, $location,$http,$ionicPopup, $firebaseObject, Auth, FURL, Utils) {
-  
- $scope.goBack = function () {
+angular.module('App').controller('menuController', function ($scope, $ionicHistory, $ionicSideMenuDelegate, $state, $cordovaOauth, $localStorage, $location, $http, $ionicPopup, $firebaseObject, Auth, FURL, Utils) {
+
+  $scope.goBack = function () {
     $location.path('/home');
- 
+
   }
- $scope.openClubs = function () {
-   $location.path('/clubs');
- }
+  $scope.openClubs = function () {
+    $location.path('/clubs');
+  }
   $scope.openFavorites = function () {
-   $location.path('/favorites');
- }
-    
-   $scope.openBars = function () {
-        $location.path('/bars');
+    $location.path('/favorites');
   }
-  
-   $scope.logOut = function () {
-        Auth.logout();
-        $location.path('/login');
-    } 
-   
-  $scope.moveItem = function(item, fromIndex, toIndex) {
+
+  $scope.openBars = function () {
+    
+    localStorage.setItem('tag', 'rap');
+
+    $location.path('/bars');
+  }
+
+  $scope.logOut = function () {
+    Auth.logout();
+    $location.path('/login');
+  }
+
+  $scope.moveItem = function (item, fromIndex, toIndex) {
     $scope.items.splice(fromIndex, 1);
     $scope.items.splice(toIndex, 0, item);
   };
-  
-  $scope.onItemDelete = function(item) {
+
+  $scope.onItemDelete = function (item) {
     $scope.items.splice($scope.items.indexOf(item), 1);
   };
-  
+
   $scope.items = [
     { id: 0 },
     { id: 1 },
-    
+
 
   ];
 }
